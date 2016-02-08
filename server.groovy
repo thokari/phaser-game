b8a003385e0ee28b2f8904dd5ad49e38e333e515
Thomas Hirsch <thokari@posteo.de>
2016-02-08 21:36:08 +0100

    server roundtrip

--- server.groovy
+++ server.groovy
@@ -1,9 +1,49 @@
 import io.vertx.groovy.ext.web.handler.StaticHandler
+import io.vertx.groovy.ext.web.handler.sockjs.SockJSHandler
 import io.vertx.groovy.ext.web.Router
 
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
+def sockJSHandler = SockJSHandler.create(vertx, [:])
+
+def inboundPermitted = [
+  address: 'server.game'
+]
+def outboundPermitted = [
+  address: 'browser.game'
+]
+def options = [
+  inboundPermitteds: [
+    inboundPermitted
+  ],
+  outboundPermitteds: [
+    outboundPermitted
+  ]
+]
+sockJSHandler.bridge(options)
+router.route('/eventbus/*').handler(sockJSHandler)
+
+def commands = []
+
+def eb = vertx.eventBus()
+eb.consumer 'server.game', { msg ->
+  def body = msg.body()
+  if ('cmd' == body.action) {
+    commands.addAll body.commands
+  }
+  if ('join' == body.action) {
+    msg.reply([status: 'ok'])
+  }
+}
+
+vertx.setPeriodic(100, {
+    if (commands.size() > 0) {
+      def message = [ commands: commands ]
+      println 'sending some commands'
+      eb.send('browser.game', message)
+      commands = []
+    }
+})
 
 router.route('/*').handler(StaticHandler.create().setCachingEnabled(false))
-
 server.requestHandler(router.&accept).listen(8080)
--- webroot/scripts/main.js
+++ webroot/scripts/main.js
@@ -1,13 +1,96 @@
-requirejs(['/scripts/vertx-eventbus.js', '/scripts/phaser.min.js'], function(EventBus, Phaser) {
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+var eb = new EventBus('http://localhost:8080/eventbus')
+var commandQueue = []
+var updateQueue = []
+var playerId = generateUUID()
+var game, sprite
 
-    function preload () {
-        game.load.image('logo', '/images/phaser.png');
+eb.onopen = function() {
+  console.log('Starting player ' + playerId);
+  eb.registerHandler('browser.game', function (err, msg) {
+    var body = msg.body
+    updateQueue = updateQueue.concat(body.commands)
+  });
+  eb.send('server.game', {
+    'action': 'join',
+    'playerId': playerId
+  }, function (result) {
+    game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {
+      preload: preload,
+      create: create,
+      update: update,
+      render: render
+    });
+  });
+}
+
+function preload() {
+  game.load.image('arrow', '/images/arrow.png');
+}
+
+function render () {
+  game.debug.spriteInfo(sprite, 32, 32);
+}
+
+function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'arrow');
+  sprite.anchor.setTo(0.5, 0.5);
+
+  game.physics.enable(sprite, Phaser.Physics.ARCADE);
+  sprite.body.allowRotation = false;
+
+  var commandTimer = function () {
+    setTimeout(function () {
+      if (commandQueue.length > 0) {
+        console.log('sending something', JSON.stringify(commandQueue))
+        eb.send('server.game', {
+          action: 'cmd',
+          commands: commandQueue
+        })
+        commandQueue = []
+      } else {
+        console.log('nothing to send', commandQueue)
+      }
+      commandTimer()
+    }, 100)
+  }
+  commandTimer()
+}
+
+function update() {
+
+  var updateVal = updateQueue.shift();
+  while (updateVal) {
+    console.log(updateVal)
+    if ('down' == updateVal.input) {
+      console.log('about to move')
+      sprite.rotation = game.physics.arcade.moveToXY(sprite, updateVal.x, updateVal.y, 60, 1000);
+    } else {
+      sprite.body.velocity.setTo(0, 0);
     }
-    function create () {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-    }
+    updateVal = updateQueue.shift();
+  }
 
+  if (game.input.mousePointer.isDown) {
+    commandQueue.push({
+      playerId: playerId,
+      input: 'down',
+      x: game.input.activePointer.x,
+      y: game.input.activePointer.y
+    })
+  }
+}
 
-});
+function generateUUID() {
+  var d = new Date().getTime();
+  if (window.performance && typeof window.performance.now === "function") {
+    d += performance.now(); //use high-precision timer if available
+  }
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (d + Math.random() * 16) % 16 | 0;
+    d = Math.floor(d / 16);
+    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+  return uuid;
+}
